e50c23aae432c91c737c645c244baeb8a7d5a870
slaFFik <slava.abakumov@gmail.com>
2013-08-24 00:14:42 +0300

    working BPGE Pro Search Extension

--- bpge-search-admin.php
+++ bpge-search-admin.php
@@ -1,1 +1,187 @@
 <?php
+class BPGE_ADMIN_SEARCH extends BPGE_ADMIN_TAB{
+    // position is used to define where exactly this tab will appear
+    var $position = 35;
+    // slug that is used in url to access this tab
+    var $slug     = 'search';
+    // title is used as a tab name
+    var $title    = null;
+
+    function __construct(){
+        $this->title = __('Search', 'bpge');
+
+        parent::__construct();
+    }
+
+    function header_title_attach(){
+        return $this->title;
+    }
+
+    /**
+     * Create sections of options
+     */
+    function register_sections(){
+        add_settings_field('search_prepare',
+            __('Prepare Search', 'bpge'),
+            array($this, 'display_prepare'),
+            $this->slug,
+            $this->slug . '_settings');
+        add_settings_field('search_fields',
+            __('Search for Fields', 'bpge'),
+            array($this, 'display_fields'),
+            $this->slug,
+            $this->slug . '_settings');
+        add_settings_field('search_pages',
+            __('Search for Pages', 'bpge'),
+            array($this, 'display_pages'),
+            $this->slug,
+            $this->slug . '_settings');
+        add_settings_field('field_2_link',
+            __('Fields to Links', 'bpge'),
+            array($this, 'display_f2l'),
+            $this->slug,
+            $this->slug . '_settings');
+    }
+
+    /**
+     * Display the tab description
+     */
+    function display(){
+        echo '<p class="description">'. __('Control the search options of groups fields and pages.', 'bpge') .'</p>';
+    }
+
+    /**
+     * Preparaion section
+     */
+    function display_prepare(){ ?>
+        <p>
+            <?php _e('Currently you need to prepare the data to be searchable. Use the button below for this purpose. This should be done only once.', 'bpge'); ?>
+        </p>
+
+        <input type="submit" id="bpge_search_prepare" class="button" name="bpge_search_prepare" value="<?php _e('Prepare Data', 'bpge'); ?>" />
+        <?php
+    }
+
+    /**
+     * Options for fields
+     */
+    function display_fields(){ ?>
+        <p>
+            <?php _e('Would you like to enable sitewide search for data entered in groups fields?'); ?>
+        </p>
+
+        <?php
+        if(!isset($this->bpge['search_fields']) || empty($this->bpge['search_fields']))
+            $this->bpge['search_fields'] = 'off';
+        ?>
+
+        <ul>
+            <li><label>
+                <input type="radio" <?php checked('on', $this->bpge['search_fields']); ?> name="bpge_search_fields" value="on" />&nbsp;
+                <?php _e('Enable', 'bpge'); ?>
+            </label></li>
+            <li><label>
+                <input type="radio" <?php checked('off', $this->bpge['search_fields']); ?> name="bpge_search_fields" value="off" />&nbsp;
+                <?php _e('Disable', 'bpge'); ?>
+            </label></li>
+
+        </ul>
+        <?php
+    }
+
+    /**
+     * Options for pages
+     */
+    function display_pages(){ ?>
+        <p>
+            <?php _e('Would you like to enable sitewide search for data entered in groups pages?'); ?>
+        </p>
+
+        <?php
+        if(!isset($this->bpge['search_pages']) || empty($this->bpge['search_pages']))
+            $this->bpge['search_pages'] = 'off';
+        ?>
+
+        <ul>
+            <li><label>
+                <input type="radio" <?php checked('on', $this->bpge['search_pages']); ?> name="bpge_search_pages" value="on" />&nbsp;
+                <?php _e('Enable', 'bpge'); ?>
+            </label></li>
+            <li><label>
+                <input type="radio" <?php checked('off', $this->bpge['search_pages']); ?> name="bpge_search_pages" value="off" />&nbsp;
+                <?php _e('Disable', 'bpge'); ?>
+            </label></li>
+
+        </ul>
+        <?php
+    }
+
+    /**
+     *
+     */
+    function display_f2l(){
+        echo '<p>';
+            _e('Would you like to convert custom fields values in all groups to links?', 'bpge'); echo '<br />';
+            _e('Using that links people will be able to search for groups with the same fields values.', 'bpge');
+        echo '</p>';
+
+        if(!isset($this->bpge['field_2_link']) || empty($this->bpge['field_2_link']))
+            $this->bpge['field_2_link'] = 'no';
+
+        echo '<ul>';
+            echo '<li><label><input type="radio" name="bpge_field_2_link" '.checked($this->bpge['field_2_link'], 'yes', false).' value="yes">&nbsp'.__('Enable','bpge').'</label></li>';
+            echo '<li><label><input type="radio" name="bpge_field_2_link" '.checked($this->bpge['field_2_link'], 'no', false).' value="no">&nbsp'.__('Disable','bpge').'</label></li>';
+        echo '</ul>';
+
+        echo '<p class="description">'. __('Note: this option uses the same logic as members profiles fields auto-linking.') .'</p>';
+
+    }
+
+    /**
+     * Save everything
+     */
+    function save(){
+        if ( isset($_POST['bpge_search_fields']) ) {
+            $this->bpge['search_fields'] = $_POST['bpge_search_fields'] == 'on' ? 'on' : 'off';
+        }
+
+        if ( isset($_POST['bpge_search_pages']) ) {
+            $this->bpge['search_pages'] = $_POST['bpge_search_pages'] == 'on' ? 'on' : 'off';
+        }
+
+        if ( isset($_POST['bpge_field_2_link']) ) {
+            $this->bpge['field_2_link'] = $_POST['bpge_field_2_link'] == 'yes' ? 'yes' : 'no';
+        }
+
+        if( isset($_POST['bpge_search_prepare']) ) {
+            // get all data from groupmeta
+            global $wpdb, $bp;
+
+            // get all groups meta - group id and associated top level `gpages` page
+            $meta = $wpdb->get_results("SELECT group_id, meta_value AS `data`
+                                        FROM {$bp->groups->table_name_groupmeta}
+                                        WHERE meta_key = 'bpge'");
+            $type = BPGE_GPAGES;
+            foreach ((array)$meta as $data) {
+                $data->data = maybe_unserialize($data->data);
+                foreach($data->data as $k => $v){
+                    if($k == 'gpage_id'){
+                        // $v = post_id (gpage associated with the group)
+                        // now we need to get all its subpages and provide group_id for them too
+                        update_post_meta($v, 'group_id', $data->group_id);
+                        $subpages = $wpdb->get_col("SELECT ID FROM {$wpdb->posts}
+                                                    WHERE post_type = '{$type}'
+                                                      AND post_parent = '{$v}'");
+                        foreach((array)$subpages as $subpage_id){
+                            update_post_meta($subpage_id, 'group_id', $data->group_id);
+                        }
+                    }
+                }
+            }
+        }
+
+        bp_update_option('bpge', $this->bpge);
+    }
+}
+
+return new BPGE_ADMIN_SEARCH;
--- bpge-search.php
+++ bpge-search.php
@@ -1,14 +1,126 @@
 <?php
 /*
 Plugin Name: BuddyPress Groups Extras Pro - Search
-Plugin URI: http://ovirium.com/portfolio/bp-groups-extras/
+Plugin URI: http://ovirium.com/downloads/bp-groups-extras-pro-search/
 Description: Adding extra fields and pages, menu sorting and other missing functionality to groups
-Version: 1.1
+Version: 1.2
 Author: slaFFik
 Author URI: http://ovirium.com/
 */
 
-add_action('admin_init', 'bpges_admin_init');
-function bpges_admin(){
+if(!defined('BPGE_PRO')){
+    define('BPGE_PRO', true);
+}
+
+define('BPGE_PRO_SEARCH', true);
+define('BPGE_PRO_SEARCH_VER', '1.2');
+
+/**
+ * Options for admin area
+ */
+add_action('admin_init', 'bpges_admin_init', 999);
+function bpges_admin_init(){
     include(dirname(__FILE__).'/bpge-search-admin.php');
+}
+
+add_action('plugins_loaded', 'bpges_init', 999);
+function bpges_init(){
+    add_filter('bp_groups_get_paged_groups_sql', 'bpges_search_add_paged', 1, 2);
+    add_filter('bp_groups_get_total_groups_sql', 'bpges_search_add_total', 1, 2);
+}
+
+
+/**
+ * Search in posts and fields for group IDs
+ */
+function bpges_search_get_groups(){
+    global $wpdb, $bpge;
+
+    // get the search terms
+    if (isset($_REQUEST['search_terms'])) {
+        // ajax
+        $search_terms = esc_sql( like_escape( $_REQUEST['search_terms'] ) );
+    }elseif (isset($_REQUEST['s'])){
+        // url-based
+        $search_terms = esc_sql( like_escape( $_REQUEST['s'] ) );
+    }else{
+        return false;
+    }
+
+    $group_ids = $pages_group_ids = $fields_group_ids = array();
+
+    // get group_ids from gpages that are relevant to this search
+    if( isset($bpge['search_pages']) && $bpge['search_pages'] == 'on' ) {
+        $type = BPGE_GPAGES;
+        $pages_group_ids = $wpdb->get_col($wpdb->prepare("SELECT pm.meta_value AS group_id
+                                            FROM {$wpdb->postmeta} AS pm
+                                            LEFT JOIN {$wpdb->posts} AS p ON p.ID = pm.post_id
+                                            WHERE pm.meta_key = 'group_id'
+                                              AND p.post_status = 'publish'
+                                              AND p.post_type = '%s'
+                                              AND p.post_parent > 0
+                                              AND (p.post_title LIKE '%%%s%%'
+                                                OR p.post_content LIKE '%%%s%%'
+                                              )", $type, $search_terms, $search_terms ) );
+    }
+
+    // get groups_ids from fields that are relevant to this search
+    if( isset($bpge['search_fields']) && $bpge['search_fields'] == 'on' ) {
+        $type = BPGE_GFIELDS;
+        $fields_group_ids = $wpdb->get_col($wpdb->prepare("SELECT DISTINCT (post_parent) AS group_id
+                                    FROM wp_posts
+                                    WHERE post_type = '%s'
+                                      AND post_status = 'publish'
+                                      AND post_parent > 0
+                                      AND post_content LIKE '%%%s%%';", $type, $search_terms));
+    }
+
+    // merge groups from 2 types of searches
+    $group_ids = array_merge($pages_group_ids, $fields_group_ids);
+
+    return $group_ids;
+}
+
+/**
+ * Modify pages search results
+ */
+// add_filter('bp_groups_get_paged_groups_sql', 'bpges_search_add_paged', 1, 2);
+function bpges_search_add_paged($sql_str, $sql_arr){
+    if( !isset($sql_arr['search']) )
+        return $sql_str;
+
+    // get all groups that have pages/fiels that are good for this search
+    $group_ids = bpges_search_get_groups();
+
+    if(!empty($group_ids)){
+        $include = 'g.ID IN ('. implode(',', $group_ids) .')';
+
+        // modify the query to get search working with groups pages
+        $sql_arr['search']  = str_replace('g.name LIKE', $include . ' OR g.name LIKE', $sql_arr['search']);
+    }
+
+    return join( ' ', (array) $sql_arr );
+}
+
+/**
+ * Modify total search results (for pagination and counters)
+ */
+// add_filter('bp_groups_get_total_groups_sql', 'bpges_search_add_total', 1, 2);
+function bpges_search_add_total($sql_str, $sql_arr){
+    // check that we are in a search
+    $pos = strpos($sql_str, 'g.name LIKE');
+    if ($pos === false)
+        return $sql_str;
+
+    // get all groups that have pages/fiels that are good for this search
+    $group_ids = bpges_search_get_groups();
+
+    if(!empty($group_ids)){
+        $include = 'g.ID IN ('. implode(',', $group_ids) .')';
+
+        // insert it into search
+        $sql_str = str_replace('g.name LIKE', $include . ' OR g.name LIKE', $sql_str);
+    }
+
+    return $sql_str;
 }
